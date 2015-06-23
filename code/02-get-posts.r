86a9738f6bf00743bf2dd59f654759cce93bbf9d
François <briatte@gmail.com>
2015-06-23 09:25:54 +0200

    final downloader bugfixes

--- code/02-get-posts.r
+++ code/02-get-posts.r
@@ -2,28 +2,32 @@
 # 2. get a large random sample of all blog posts
 #
 
+library(dplyr)
 library(readr)
 
 dir.create("html", showWarnings = FALSE)
 
-a = read_csv("data/posts.csv")
+a = read_csv("data/posts.csv") %>%
+  unique
 
-# select well-formed article addresses
-b = with(a, paste0(blog, slug))
-b = b[ grepl("^http://[a-z0-9]+.hypotheses.org/\\d+$", b) ]
+# build full post URLs
+a = with(a, paste0(blog, slug))
+
+# select only well-formed
+a = a[ grepl("^http://[a-z0-9]+.hypotheses.org/\\d+$", a) ]
 
 # fraction of full sample to download
 s = 1/4
 
 # list of file names
-l = gsub("http://(.*)\\.hypotheses\\.org/(.*)", "html/\\1.\\2.html", b)
-names(l) = b
+l = gsub("http://(.*)\\.hypotheses\\.org/(.*)", "html/\\1.\\2.html", a)
+names(l) = a
 
 # select fraction of full sample
-k = round(s * nrow(a)) - sum(file.exists(l))
+k = round(s * length(a)) - sum(file.exists(l))
 
 # sample that many missing files
-k = sample(b[ !file.exists(l) ], k)
+k = sample(a[ !file.exists(l) ], k)
 
 while(length(k) > 0) {
 
@@ -52,9 +56,9 @@ while(length(k) > 0) {
     cat(".. removed", length(null), "empty file(s)\n")
 
   # select fraction of full sample
-  k = round(s * nrow(a)) - sum(file.exists(l))
+  k = round(s * length(a)) - sum(file.exists(l))
 
   # sample that many missing files
-  k = sample(b[ !file.exists(l) ], k)
+  k = sample(a[ !file.exists(l) ], k)
 
 }
